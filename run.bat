cfce8f6486a36e184e62a8eadba9b431f1bba928
Guzman <eguzman6@csus.edu>
2023-04-07 22:54:51 -0700

    server run.bat updated

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -Dsun.java2d.d3d=false -Dsun.java2d.uiScale=1 MazeGameServer.NetworkingServer 6025 UDP
+java -Dsun.java2d.d3d=false -Dsun.java2d.uiScale=1 MazeGameServer.NetworkingServer 6010 UDP
