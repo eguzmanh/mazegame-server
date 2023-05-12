268f7430533c335aad84b6216cdceb414a47298d
aespinosa05 <aarongr.espinosa@gmail.com>
2023-05-11 18:53:39 -0700

    Changes for NPC

--- MazeGameServer/NPC.java
+++ MazeGameServer/NPC.java
@@ -1,14 +1,22 @@
 package MazeGameServer;
 
+import org.joml.Vector3f;
+
+import tage.rml.Vector3;
+
 public class NPC { 
     double locationX, locationY, locationZ;
     double dir = 0.1;
     double size = 1.0;
+    boolean seePlyr = false, seeGhst = false;
+    float speed = 0.01f;
+    Vector3f npcLocation, targetLocAv, targetLocGhost;
     
     public NPC() { 
         locationX=0.0;
         locationY=0.0;
         locationZ=0.0;
+        npcLocation = new Vector3f();
     }
 
     public void randomizeLocation(int seedX, int seedZ) { 
@@ -25,9 +33,29 @@ public class NPC {
     public void getSmall() { size=1.0; }
     public double getSize() { return size; }
 
+    public void setTargetLocationAvatar(Vector3f plyrPos){ targetLocAv = plyrPos; }
+    public void setTargetLocationGhost(Vector3f ghostPos){ targetLocGhost = ghostPos; }
+
+    public void setSeePlyr(boolean b){ seePlyr = b; }
+    public void setSeeGhost(boolean b){ seeGhst = b; }
+
     public void updateLocation() { 
-        if (locationX > 10) dir=-0.1;
-        if (locationX < -10) dir=0.1;
-        locationX = locationX + dir;
+        if(seeGhst || seePlyr){
+            if(seePlyr){
+                Vector3f direction = new Vector3f();
+                targetLocAv.sub(npcLocation, direction).normalize();
+                direction.mul(speed);
+                npcLocation.add(direction);
+            } else if (seeGhst){
+                Vector3f direction = new Vector3f();
+                targetLocGhost.sub(npcLocation, direction).normalize();
+                direction.mul(speed);
+                npcLocation.add(direction);
+            }
+        } else if(seePlyr == false && seeGhst == false){
+            if (locationX > 10) dir=-0.1;
+            if (locationX < -10) dir=0.1;
+            locationX = locationX + dir;
+        }
     } 
 }
